304df142535672800616dd0507acb3f235e12e64
anhhhhe170853 <144606108+Anhhhe170853@users.noreply.github.com>
2024-10-29 08:11:11 +0700

    Create AppLocation.java

---
+++ app/src/main/java/com/example/weather_app/model/AppLocation.java
@@ -0,0 +1,98 @@
+package com.example.weatherapp.model;
+
+import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.TimeZone;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class AppLocation implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String state;
+    private String country;
+
+    public AppLocation() {
+    }
+
+    public AppLocation(String name, double latitude, double longitude, String state,
+        String country) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.state = state;
+        this.country = country;
+    }
+
+    public AppLocation(long id, String name, double latitude, double longitude, String state,
+        String country) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.state = state;
+        this.country = country;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    
